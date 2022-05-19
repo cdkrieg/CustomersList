98d5096be3065d200b266e0a87fb34974894acb0
Christopher Krieg <cdkrieg@outlook.com>
2022-05-18 21:03:23 -0500

    moved functionality from ProfilePage to EditProfile component. Added cancel button to edit profile

---
+++ frontend/src/components/EditProfile/EditProfile.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect, useContext } from 'react';
+import AuthContext from "../../context/AuthContext";
+import { Form, Button, Alert, FormCheck } from "react-bootstrap";
+import { useNavigate } from 'react-router';
+import stateArray from "../../components/StateList/StateList";
+import useCustomForm from "../../hooks/UseCustomForm";
+
+const EditProfile = () => {
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate()
+    const defaultValues = {
+      name: user.name || "",
+      streetAddressLine1: user.streetAddressLine1,
+      streetAddressLine2: user.streetAddressLine2 || "",
+      city: user.city,
+      state: user.state,
+      zipCode: user.zipCode,
+    };
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(
+      defaultValues
+    );
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [passwordValidation, setPasswordValidation] = useState("notValid");
+    const [showPasswordAlert1, setShowPasswordAlert1] = useState(false);
+    const [showPasswordAlert2, setShowPasswordAlert2] = useState(false);
+    const [showStateAlert, setShowStateAlert] = useState(false);
+    const [checked, setChecked] = useState(false)
+  
+    function passwordCheck(event) {
+      event.preventDefault()
+      if (formData.password && formData.password !== passwordConfirm) {
+        return setShowPasswordAlert1(true);
+      } else if (
+        formData.password &&
+        formData.password !== "" &&
+        formData.password.length < 8
+      ) {
+        return setShowPasswordAlert2(true);
+      } else if (formData.state === "Choose State") {
+        console.log(formData.state);
+        return setShowStateAlert(true);
+      } else {
+        return ((event)=>handleSubmit(event))
+        
+      }
+    }
+  
+    useEffect(() => {
+      if (formData.password) {
+        if (formData.password !== passwordConfirm) {
+          setPasswordValidation("notValid");
+        } else {
+          setPasswordValidation("valid");
+          setShowPasswordAlert1(false);
+        }
+      }
+    }, [passwordConfirm]);
+  
+
+
+    return ( 
+        <div>
+            <Form className='form' onSubmit={(event) => passwordCheck(event)}>
+        <Form.Control
+          name='name'
+          value={formData.name}
+          placeholder={"Enter first and last name (optional)"}
+          onChange={handleInputChange}
+        />
+        <Form.Control
+          name='streetAddressLine1'
+          value={formData.streetAddressLine1}
+          onChange={handleInputChange}
+        />
+        <Form.Control
+          name='streetAddressLine2'
+          value={formData.streetAddressLine2}
+          onChange={handleInputChange}
+        />
+
+        <Form.Select
+          name='state'
+          value={formData.state}
+          onChange={(event) => handleInputChange(event)}>
+          {stateArray.map((item, index) => {
+            return <option key={index}>{item.value}</option>;
+          })}
+        </Form.Select>
+        <Form.Control
+          name='city'
+          value={formData.city}
+          onChange={handleInputChange}
+          id="profileCity"
+        />
+        <Form.Control
+          name='zipCode'
+          value={formData.zipCode}
+          onChange={handleInputChange}
+        />
+        <Form.Check type="checkbox" label="Check to change password" onChange={()=>{setChecked(!checked);defaultValues.password=""}}/>
+        {checked && <Form.Control
+          name='password'
+          placeholder='Enter new password'
+          type="password"
+          autoComplete='off'
+          value={formData.password}
+          onChange={handleInputChange}
+        />}
+        {checked && <Form.Control
+          name='passwordConfirm'
+          className={passwordValidation}
+          autoComplete='off'
+          type="password"
+          placeholder='Enter password to confirm'
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />}
+        
+          {checked && showPasswordAlert1 && (
+            <Alert
+              className='alert'
+              variant='danger'
+              onClose={() => setShowPasswordAlert1(false)}
+              dismissible>
+              <Alert.Heading>Passwords do not match!</Alert.Heading>
+            </Alert>
+          )}
+   
+          {checked && showPasswordAlert2 && (
+            <Alert
+              className='alert'
+              variant='danger'
+              onClose={() => setShowPasswordAlert2(false)}
+              dismissible>
+              <Alert.Heading>Password too short!</Alert.Heading>Passwords must
+              contain at least 8 characters.
+            </Alert>
+          )}
+       <p></p>
+        <div className='submitButton'>
+          <Button
+            id='submitButton'
+            type='submit'
+            variant='dark'
+            onSubmit={(event) => passwordCheck(event)}>
+            Submit
+          </Button>
+          <Button type='cancel' onClick={()=>navigate('/')} variant='dark'>Cancel</Button>
+        </div>
+      </Form>
+        </div>
+
+     );
+}
+ 
+export default EditProfile;
--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,149 +1,15 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
+import EditProfile from "../../components/EditProfile/EditProfile";
 import AuthContext from "../../context/AuthContext";
-import { Form, Button, Alert, FormCheck } from "react-bootstrap";
-import stateArray from "../../components/StateList/StateList";
-import useCustomForm from "../../hooks/UseCustomForm";
 import "./ProfilePage.css";
 
 const ProfilePage = () => {
-  const { user } = useContext(AuthContext);
-  const defaultValues = {
-    name: user.name || "",
-    streetAddressLine1: user.streetAddressLine1,
-    streetAddressLine2: user.streetAddressLine2 || "",
-    city: user.city,
-    state: user.state,
-    zipCode: user.zipCode,
-  };
-  const [formData, handleInputChange, handleSubmit] = useCustomForm(
-    defaultValues
-  );
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [passwordValidation, setPasswordValidation] = useState("notValid");
-  const [showPasswordAlert1, setShowPasswordAlert1] = useState(false);
-  const [showPasswordAlert2, setShowPasswordAlert2] = useState(false);
-  const [showStateAlert, setShowStateAlert] = useState(false);
-  const [checked, setChecked] = useState(false)
-
-  function passwordCheck(event) {
-    event.preventDefault()
-    if (formData.password && formData.password !== passwordConfirm) {
-      return setShowPasswordAlert1(true);
-    } else if (
-      formData.password &&
-      formData.password !== "" &&
-      formData.password.length < 8
-    ) {
-      return setShowPasswordAlert2(true);
-    } else if (formData.state === "Choose State") {
-      console.log(formData.state);
-      return setShowStateAlert(true);
-    } else {
-      return ((event)=>handleSubmit(event))
-      
-    }
-  }
-
-  useEffect(() => {
-    if (formData.password) {
-      if (formData.password !== passwordConfirm) {
-        setPasswordValidation("notValid");
-      } else {
-        setPasswordValidation("valid");
-        setShowPasswordAlert1(false);
-      }
-    }
-  }, [passwordConfirm]);
+  const {user} = useContext(AuthContext)
 
   return (
     <div className='container-profilePage'>
-      <Form className='form' onSubmit={(event) => passwordCheck(event)}>
-        <Form.Control
-          name='name'
-          value={formData.name}
-          placeholder={"Enter first and last name (optional)"}
-          onChange={handleInputChange}
-        />
-        <Form.Control
-          name='streetAddressLine1'
-          value={formData.streetAddressLine1}
-          onChange={handleInputChange}
-        />
-        <Form.Control
-          name='streetAddressLine2'
-          value={formData.streetAddressLine2}
-          onChange={handleInputChange}
-        />
-
-        <Form.Select
-          name='state'
-          value={formData.state}
-          onChange={(event) => handleInputChange(event)}>
-          {stateArray.map((item, index) => {
-            return <option key={index}>{item.value}</option>;
-          })}
-        </Form.Select>
-        <Form.Control
-          name='city'
-          value={formData.city}
-          onChange={handleInputChange}
-          id="profileCity"
-        />
-        <Form.Control
-          name='zipCode'
-          value={formData.zipCode}
-          onChange={handleInputChange}
-        />
-        <Form.Check type="checkbox" label="Check to change password" onChange={()=>{setChecked(!checked);defaultValues.password=""}}/>
-        {checked && <Form.Control
-          name='password'
-          placeholder='Enter new password'
-          type="password"
-          autoComplete='off'
-          value={formData.password}
-          onChange={handleInputChange}
-        />}
-        {checked && <Form.Control
-          name='passwordConfirm'
-          className={passwordValidation}
-          autoComplete='off'
-          type="password"
-          placeholder='Enter password to confirm'
-          value={passwordConfirm}
-          onChange={(e) => setPasswordConfirm(e.target.value)}
-        />}
-        
-          {checked && showPasswordAlert1 && (
-            <Alert
-              className='alert'
-              variant='danger'
-              onClose={() => setShowPasswordAlert1(false)}
-              dismissible>
-              <Alert.Heading>Passwords do not match!</Alert.Heading>
-            </Alert>
-          )}
-   
-          {checked && showPasswordAlert2 && (
-            <Alert
-              className='alert'
-              variant='danger'
-              onClose={() => setShowPasswordAlert2(false)}
-              dismissible>
-              <Alert.Heading>Password too short!</Alert.Heading>Passwords must
-              contain at least 8 characters.
-            </Alert>
-          )}
-       
-        <div className='submitButton'>
-          <Button
-            id='submitButton'
-            type='submit'
-            variant='dark'
-            onSubmit={(event) => passwordCheck(event)}>
-            Submit
-          </Button>
-        </div>
-      </Form>
+      {user && <EditProfile /> }
+      {!user && <p>User not logged in</p> }
     </div>
   );
 };
